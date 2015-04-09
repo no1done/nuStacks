eea3e8bac90d4dd8e412d94ec519459810ecf77a
no1done <ryan.s.done@gmail.com>
2015-04-09 21:54:49 +0100

    New controllers and models for base system
    New models for the base system which include:
    
    * posts
    * images
    * friends
    * followers
    * stacks
    
    And just a new post controller for now

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,17 +1,8 @@
 <?php namespace App\Http\Controllers;
 
-class HomeController extends Controller {
+use App\Post;
 
-	/*
-	|--------------------------------------------------------------------------
-	| Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller renders your application's "dashboard" for users that
-	| are authenticated. Of course, you are free to change or remove the
-	| controller as you wish. It is just here to get your app started!
-	|
-	*/
+class HomeController extends Controller {
 
 	/**
 	 * Create a new controller instance.
@@ -30,8 +21,12 @@ class HomeController extends Controller {
 	 */
 	public function index()
 	{
+		$posts = Post::all();
 
-		return view('app.home')->with([]);
+		return view('app.home')->with([
+			'posts' => $posts
+		]);
 	}
+
 
 }
---
+++ app/Post.php
@@ -0,0 +1,26 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model {
+
+	/**
+	* The database table used by the model.
+	*
+	* @var string
+	*/
+	protected $table = 'posts';
+
+	/**
+	* The attributes that are mass assignable.
+	*
+	* @var array
+	*/
+	protected $fillable = ['user_id', 'post', 'published', 'deleted'];
+
+	public function user()
+	{
+		return $this->belongsTo('App\User', 'user_id')->select(['firstname', 'surname']);
+	}
+
+}
